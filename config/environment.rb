cabdb9bdbd399e64d03e4c612ca3b77c11036c94
john <john@c2b4ead4-481d-0410-ac63-b02590f85e3f>
2006-09-12 22:10:23 +0000

    removed rails, updated to 1.1.6
    git-svn-id: svn+ssh://mule/home/john/newsniffer/svn/trunk@4 c2b4ead4-481d-0410-ac63-b02590f85e3f

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.1.4'
+RAILS_GEM_VERSION = '1.1.6'
 
 ENV['ADMIN_PASSWORD'] = "dogmacatma"
 
