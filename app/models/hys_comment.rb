7450c5b7658ed40d206ad6e254713a83d785ee23
john <john@c2b4ead4-481d-0410-ac63-b02590f85e3f>
2006-10-25 12:31:29 +0000

    some docs
    git-svn-id: svn+ssh://mule/home/john/newsniffer/svn/trunk@18 c2b4ead4-481d-0410-ac63-b02590f85e3f

--- app/models/hys_comment.rb
+++ app/models/hys_comment.rb
@@ -1,16 +1,21 @@
+# The HysComment object represents a BBC Have Your Say comment, usually associated
+# with a HysThread object.
 class HysComment < ActiveRecord::Base
   belongs_to :hys_thread
 #  has_many :votes, :foreign_key => 'relation_id', :conditions => ["class = ?", self.class_name]
   validates_uniqueness_of :bbcid
 
+  # Mark the specified (by bbcid) comment as not censored in the database
   def self.uncensor(bbcid)
     self.update_all "censored = 1", "bbcid = #{bbcid}"
   end
   
+  # Mark the specified (by bbcid) comment as censored in the database 
   def self.censor(bbcid)
     self.update_all "censored = 0", "bbcid = #{bbcid}"
   end
 
+  # Return the url for this comment on the bbc website
   def url
     "http://newsforums.bbc.co.uk/nol/thread.jspa?messageID=#{bbcid}##{bbcid}"
   end
--- app/models/hys_thread.rb
+++ app/models/hys_thread.rb
@@ -1,3 +1,7 @@
+# The HysThread model represents a BBC Have Your Say thread.
+# The +++censored+++ relation returns all associated HysComments that were censored.
+# The +++hardcensored+++ relation returns all associated HysComments that have remained censored for
+# a period of time.
 class HysThread < ActiveRecord::Base
   attr_accessor :ccount
   has_many :hys_comments
@@ -6,14 +10,19 @@ class HysThread < ActiveRecord::Base
   has_many :censored, :class_name => 'HysComment', :conditions => ['censored = 0']
   has_many :hardcensored, :class_name => 'HysComment', :conditions => ['censored = 0 and updated_at < (now() - INTERVAL 16 minute)']
 
+  # Return an array of ids of all HysComments from this 
+  # thread modified since the time parameter
   def comment_ids_since(time)
     hys_comments.find(:all, :conditions => ['modified_at >= ?', time]).collect { |c| c.bbcid }
   end
   
+  # Return an array of ids of censored HysComments from this
+  # thread modified since the time parameter
   def censored_comment_ids_since(time)
     hys_comments.find(:all, :conditions => ['censored = 0 and modified_at >= ?', time]).collect { |c| c.bbcid }
   end
 
+  # Return the url to the bbc website for this thread
   def url
     "http://newsforums.bbc.co.uk/nol/thread.jspa?threadID=#{bbcid}"
   end
