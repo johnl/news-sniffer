f06f1dccb02d0c53802e54e43a306a59f9ba615f
john <john@c2b4ead4-481d-0410-ac63-b02590f85e3f>
2006-10-25 20:38:06 +0000

    changed censor/uncensor to trigger after_save filters
    git-svn-id: svn+ssh://mule/home/john/newsniffer/svn/trunk@20 c2b4ead4-481d-0410-ac63-b02590f85e3f

--- app/models/hys_comment.rb
+++ app/models/hys_comment.rb
@@ -5,18 +5,21 @@ class HysComment < ActiveRecord::Base
 #  has_many :votes, :foreign_key => 'relation_id', :conditions => ["class = ?", self.class_name]
   validates_uniqueness_of :bbcid
 
-  # Mark the specified (by bbcid) comment as not censored in the database
+  # Mark the specified (by bbcid) comment as not censored and save
   def self.uncensor(bbcid)
-    self.update_all "censored = 1", "bbcid = #{bbcid}"
+    c = self.find_by_bbcid(bbcid, :include => :hys_thread)
+    c.update_attribute(:censored, 1)
   end
   
-  # Mark the specified (by bbcid) comment as censored in the database 
+  # Mark the specified (by bbcid) comment as censored and save
   def self.censor(bbcid)
-    self.update_all "censored = 0", "bbcid = #{bbcid}"
+    c = self.find_by_bbcid(bbcid, :include => :hys_thread)
+    c.update_attribute(:censored, 0)
   end
 
   # Return the url for this comment on the bbc website
   def url
     "http://newsforums.bbc.co.uk/nol/thread.jspa?messageID=#{bbcid}##{bbcid}"
   end
+
 end
