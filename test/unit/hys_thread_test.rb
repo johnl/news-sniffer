76cacb39bc475065b7661f1846e95b8c65f5be9a
john <john@c2b4ead4-481d-0410-ac63-b02590f85e3f>
2006-10-29 14:48:19 +0000

    more tests
    git-svn-id: svn+ssh://mule/home/john/newsniffer/svn/trunk@42 c2b4ead4-481d-0410-ac63-b02590f85e3f

--- test/unit/hys_thread_test.rb
+++ test/unit/hys_thread_test.rb
@@ -3,6 +3,8 @@ require File.dirname(__FILE__) + '/../test_helper'
 class HysThreadTest < Test::Unit::TestCase
 
   def test_rss
+
+    # Test getting threads list from rss
     assert HysThread.delete_all
     assert HysComment.delete_all
     assert_equal 0, HysThread.count
@@ -12,14 +14,16 @@ class HysThreadTest < Test::Unit::TestCase
     assert HysThread.find_from_rss.size > 8, "didn't find enough threads from rss feed second time"
     assert_equal before, HysThread.count, "second run of find_from_rss created new threads (could be a race cond)"
     
-    #t = HysThread.find(:first, :order => 'rand()')
-    t = HysThread.find_by_bbcid(4522)
+    # Test getting associated comments from rss
+    t = HysThread.find(:first, :order => 'rand()')
     assert t.bbcid.is_a?(Fixnum), "bbcid wasn't parsed"
 
     assert_equal 0, t.hys_comments.count, "thread already has comments!"
     comments = t.find_comments_from_rss
     assert_equal comments.size, comments.uniq.size, "rss feed had duplicates"
     assert_equal comments.size, t.hys_comments.count, "comments from rss weren't created in db"
+
+    # Test new comments detection
     comments.first.destroy
     assert_equal comments.size - 1, t.hys_comments.count, "test comment wasn't destroyed"
     t = HysThread.find(t.id)
@@ -27,5 +31,12 @@ class HysThreadTest < Test::Unit::TestCase
     t.last_rss_pubdate = nil
     newcomments = t.find_comments_from_rss
     assert_equal comments.size, t.hys_comments.count, "deleted test comments wasn't found as new in feed"
+
+    # Test censoring
+    assert c = t.hys_comments.find(:first, :offset => 3, :order => 'bbcid', :conditions => "censored = #{NOTCENSORED}")
+    assert c.bbcid
+    assert c.censor!
+    assert c.reload
+    assert_equal CENSORED, c.censored
   end
 end
