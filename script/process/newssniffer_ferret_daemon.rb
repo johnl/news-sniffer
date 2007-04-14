2c93f403e54823f99991a33ea61ba3fed0aab092
john <john@c2b4ead4-481d-0410-ac63-b02590f85e3f>
2007-04-14 22:34:18 +0000

    debug output
    git-svn-id: svn+ssh://mule/home/john/newsniffer/svn/trunk@118 c2b4ead4-481d-0410-ac63-b02590f85e3f

--- script/process/newssniffer_ferret_daemon.rb
+++ script/process/newssniffer_ferret_daemon.rb
@@ -3,24 +3,35 @@
 require File.dirname(__FILE__) + '/../../config/environment'
 
 module Ferret::Index
-class Index
-  alias :real_search :search
-
-  def search(*args)
-    r = nil
-    b = Benchmark.measure do
-      r = real_search(*args)
-    end
-    puts b
-    r
-  end
+  class Index
+    alias :real_search :search
   
+    def search(*args)
+      r = nil
+      b = Benchmark.measure { r = real_search(*args) }
+      puts "ferret search completed in #{b.to_s}"
+      return r
+    end
+    
+  end
 end
+
+class ActionController::Caching::Fragments::FileStore
+  include DRb::DRbUndumped
+  def write(*args)
+    puts "fragment write " + args.first
+    super *args
+  end
+  def read(*args)
+    puts "fragment read " + args.first
+    super *args
+  end
 end
+
 
 services = { 
   :news_article_version_ferret => NewsArticleVersion.ferret_init_index(),
-  :fragment_cache => ActiveRecord::Base.allow_concurrency
+  :fragment_cache => ActionController::Base.fragment_cache_store
   }
 DRb.install_id_conv DRb::TimerIdConv.new
 
