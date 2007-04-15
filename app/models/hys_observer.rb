6508989749acbba52cf5dbd32d3e38059b812f60
john <john@c2b4ead4-481d-0410-ac63-b02590f85e3f>
2007-04-15 00:41:33 +0000

    expiry for hys votes
    git-svn-id: svn+ssh://mule/home/john/newsniffer/svn/trunk@129 c2b4ead4-481d-0410-ac63-b02590f85e3f

--- app/models/hys_observer.rb
+++ app/models/hys_observer.rb
@@ -1,5 +1,5 @@
 class HysObserver < ActiveRecord::Observer
-  observe HysComment, HysThread
+  observe HysComment, HysThread, Vote
 
   def expire_fragment(key)
     ActionController::Base.fragment_cache_store.delete_matched(key, nil)
@@ -10,18 +10,28 @@ class HysObserver < ActiveRecord::Observer
   end
 
   def after_update(model)
-    if model.is_a? HysComment and model.hys_thread
-      expire_fragment(/bbc\/threads/)
-      expire_fragment(/bbc\/comments/)
-      expire_fragment(/bbchysthreads\/show\/#{model.hys_thread.bbcid}/)
+
+    if model.is_a? HysComment
+      expire_fragment(/bbc\/threads\/all/)
+      expire_fragment(/bbc\/threads\/mostcensored/)
+
+      expire_fragment(/bbc\/comments\/list/)
+      if model.hys_thread
+        expire_fragment(/bbc\/threads\/show\/#{model.hys_thread.bbcid}/)
+      end
     end
+
     if model.is_a? HysThread
-      expire_fragment(/bbc\/threads/)
-      expire_fragment(/bbc\/comments/)
+      expire_fragment(/bbc\/threads\/all/)
+      expire_fragment(/bbc\/threads\/mostcensored/)
+
       expire_fragment(/bbchysthreads\/show\/#{model.bbcid}/)
+    end
+
+    if model.is_a? Vote and model.read_attribute('class') == "HysComment"
+      expire_fragment(/bbc\/comments\/recommended/)
+      expire_fragment(/bbc\/comments\/top_recommended/)
     end
   end
 
 end
-
-#HysObserver.instance
