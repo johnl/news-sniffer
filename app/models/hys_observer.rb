aa2c5d060dd98e1b4b7ba8314b8e3f45a71cc0e4
john <john@c2b4ead4-481d-0410-ac63-b02590f85e3f>
2007-04-15 00:17:31 +0000

    new expiring patterns
    git-svn-id: svn+ssh://mule/home/john/newsniffer/svn/trunk@125 c2b4ead4-481d-0410-ac63-b02590f85e3f

--- app/models/hys_observer.rb
+++ app/models/hys_observer.rb
@@ -1,19 +1,27 @@
 class HysObserver < ActiveRecord::Observer
   observe HysComment, HysThread
-  #include ActionController::Benchmarking::ClassMethods
-  include ActionController::Caching
+
+  def expire_fragment(key)
+    ActionController::Base.fragment_cache_store.delete_matched(key, nil)
+  end
 
   def self.benchmark(title, log_level = Logger::DEBUG, use_silence = true)
     yield
   end
 
   def after_update(model)
-    if model.is_a? HysComment
-      expire_fragment( "hys_thread_#{model.hys_thread.bbcid}" ) if model.hys_thread
-      #expire_fragment( "hys_comments_recommended_page_" + i.to_s )
+    if model.is_a? HysComment and model.hys_thread
+      expire_fragment(/bbc\/threads/)
+      expire_fragment(/bbc\/comments/)
+      expire_fragment(/bbchysthreads\/show\/#{model.hys_thread.bbcid}/)
+    end
+    if model.is_a? HysThread
+      expire_fragment(/bbc\/threads/)
+      expire_fragment(/bbc\/comments/)
+      expire_fragment(/bbchysthreads\/show\/#{model.bbcid}/)
     end
   end
 
 end
 
-HysObserver.instance
+#HysObserver.instance
