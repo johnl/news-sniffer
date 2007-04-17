371f5d2955cf37e4a406ec3368129981b92826a4
john <john@c2b4ead4-481d-0410-ac63-b02590f85e3f>
2007-04-17 17:50:22 +0000

    add support for easy switching between drb and non-drb mode, for test environment
    git-svn-id: svn+ssh://mule/home/john/newsniffer/svn/trunk@159 c2b4ead4-481d-0410-ac63-b02590f85e3f

--- lib/drb_service.rb
+++ lib/drb_service.rb
@@ -1,6 +1,12 @@
 module NsDrb
 
-  @@service_hash = nil
+  def self.local=(b)
+    @local = b  
+  end
+
+  def self.local?
+    @local
+  end
 
   def self.url
     config = YAML.load_file("#{RAILS_ROOT}/config/drb_service.yaml")[RAILS_ENV]
@@ -8,16 +14,23 @@ module NsDrb
     port = config["port"]
     "druby://#{host}:#{port}"
   end
+  
+  def self.services
+    #return @service_hash unless @@service_hash.nil?
+    return @service_hash ||= local? ? local_services : remote_services
+  end
 
-
-  def self.connect
+  def self.remote_services
     DRb.start_service
     DRbObject.new(nil, url)
   end
 
-  def self.services
-    return @@service_hash unless @@service_hash.nil?
-    @@service_hash = connect
+  def self.local_services
+    { 
+      :news_article_version_ferret => NewsArticleVersion.ferret_init_index(),
+      :hys_comment_ferret => HysComment.ferret_init_index(),
+      :fragment_cache => ActionController::Caching::Fragments::FileStore.new("#{RAILS_ROOT}/tmp/cache/fragment_cache/#{RAILS_ENV}")
+    }
   end
 
 end
