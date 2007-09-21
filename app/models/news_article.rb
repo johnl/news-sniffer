c8b20f66b48c1ab6f580d18efcb361f001fc192d
john <john@c2b4ead4-481d-0410-ac63-b02590f85e3f>
2007-09-21 11:02:49 +0000

    separate logging tweak errors logging
    git-svn-id: svn+ssh://mule/home/john/newsniffer/svn/trunk@200 c2b4ead4-481d-0410-ac63-b02590f85e3f

--- app/models/news_article.rb
+++ app/models/news_article.rb
@@ -7,23 +7,25 @@ class NewsArticle < ActiveRecord::Base
   validates_length_of :url, :minimum => 10
   validates_uniqueness_of :guid
 
+
+  # Retrieve the news page from the web, parse it and create a new version if detected
+  # returns the saved NewsArticleVersion
   def update_from_source
     page_data = HTTP::zget(url)
     page = page_parser.new(page_data)
     page.url = url
     return nil if page.text_hash.nil? or page.text_hash == latest_text_hash
     if source == "guardian" and versions.find_all_by_text_hash(page.text_hash).size > 0
-      logger.warn "skipping flip-flopping Guardian news article, id:#{id}"
+      NSLOG.warn "NewsArticle:skipping flip-flopping Guardian news article, id:#{id}"
       return nil
     end
-    logger.info("NewsArticle:new version found for '#{guid}'")
+    NSLOG.info "NewsArticle:new version found for '#{guid}'"
     update_attribute(:latest_text_hash, page.text_hash)
     nv = NewsArticleVersion.new
     nv.populate_from_page(page)
     versions << nv
     nv
   end
-
 
   # Return the title of the latest version of this article
   def latest_title
@@ -47,10 +49,10 @@ class NewsArticle < ActiveRecord::Base
       a.url = e[:link]
       begin
         a.save!
-        logger.info "INFO:NewsArticle:new news article found: '#{e.title}'"
+        NSLOG.info "NewsArticle:new news article found: '#{e.title}'"
         next a
       rescue ActiveRecord::RecordInvalid
-        logger.info "INFO:NewsArticle:news article '#{a.title}' not created: #{a.errors.full_messages}"
+        NSLOG.info "NewsArticle:news article '#{a.title}' not created: #{a.errors.full_messages}"
         next nil
       end
     end
@@ -58,19 +60,21 @@ class NewsArticle < ActiveRecord::Base
   end
 
   private
-  
+ 
+  # retrieve and parse the given rss feed url and return an array of SimpleRSS entry items
   def self.get_rss_entries(url)
     rssdata = HTTP::zget(url)
     begin
       rss = SimpleRSS.parse(rssdata)
       entries = rss.entries
     rescue SimpleRSSError
-      logger.error("NewsArticle:get_rss_entires:RSS malformed: #{url}")
+      NSLOG.error "NewsArticle:get_rss_entires:RSS malformed: #{url}"
       entries = []
     end
     return entries
   end
 
+  # Return the NewsPage parser for this news source
   def page_parser
     case source
       when "bbc"
