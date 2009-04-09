99548c4fbfe786efdcde43753816c9586e1c9cdd
John Leach <john@johnleach.co.uk>
2009-04-09 20:17:22 +0100

    Added older BBC News article format from 2006 and changed specs to test for that (as that is what the current code is parsing)

--- spec/models/bbc_news_page_spec.rb
+++ spec/models/bbc_news_page_spec.rb
@@ -3,17 +3,17 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe NewsPage::BbcNewsPage do
 
   before(:each) do
-    @page_data_1 ||= File.read("spec/fixtures/web_pages/7984711-A.stm.html")
-    @page_data_2 ||= File.read("spec/fixtures/web_pages/7984711-B.stm.html")
+    @page_data_1 ||= File.read("spec/fixtures/web_pages/6072486-A.stm.html")
+    @page_data_2 ||= File.read("spec/fixtures/web_pages/6072486-B.stm.html")
     @page = NewsPage::BbcNewsPage.new(@page_data_1)
   end
 
   it "should parse the title" do
-    @page.title.should == "PM Brown plays down expenses row"
+    @page.title.should == "Son-in-law remanded over killing"
   end
 
   it "should parse the date in utc" do
-    @page.date.should == Time.parse("Sun Apr 05 19:26:01 +0000 2009")
+    @page.date.should == Time.parse("Sat Oct 21 14:41:10 +0000 2006")
     @page.date.zone.should == '+00:00'
   end
 
@@ -22,7 +22,8 @@ describe NewsPage::BbcNewsPage do
   end
 
   it "should parse the page content" do
-    pending
-    @page.content.first.should == "Gordon Brown says he has more important issues than MPs' expenses to deal with as fresh controversy grew about bills submitted by his transport secretary."
+    @page.content.first.should == "<b>The son-in-law of a 73-year-old Castleford widow has been charged with her murder.</b></font>"
+    @page.content.last.should == "<font size=\"2\">He denied the charges against him through his solicitor and is due to appear at Leeds Crown Court on Friday."
+    @page.content.size.should == 5
   end
 end
