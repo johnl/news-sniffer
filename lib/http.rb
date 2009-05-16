14ee5a5facbe57e28ea90a3d2c3618e407c692c7
John Leach <john@johnleach.co.uk>
2009-05-16 01:25:39 +0100

    libcurl/curb version of zget

--- lib/http.rb
+++ lib/http.rb
@@ -15,58 +15,29 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 module HTTP
-  require 'open-uri'
-  def self.head_hash(url)
-    t = url.scan /http:\/\/([^\/]+)\/(.*)$/
-    host = t.first.first
-    path = t.first.last
-    Net::HTTP.start(host, 80) do |http|
-      begin
-        @response = http.head('/'+path)
-      rescue Timeout::Error
-				log_warn("head_hash: Timeout::Error on #{url}")
-        return false
-      end
+  require 'net/http'
+  require 'curb'
+  require 'zlib'
+  
+  def self.zget(url)
+    purl = URI.parse(url)
+
+    @c ||= Curl::Easy.new do |c|
+      c.timeout = 8
+      c.connect_timeout = 8
+      c.dns_cache_timeout = 600
+      c.enable_cookies = false
+      c.follow_location = true
+      c.headers["User-Agent"] = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.4) Gecko/20060508 Firefox/1.5.0.4'
+      c.headers["Accept-encoding"] = 'gzip, deflate'
     end
-    @response.to_hash
+    @c.url = url
+    @c.perform
+    raw_body = StringIO.new(@c.body_str)
+    body = nil
+    body = Zlib::GzipReader.new(raw_body) rescue nil
+    body = Zlib::Deflate.new(raw_body) rescue nil unless body
+    (body || raw_body).read
   end
   
-  def self.remote_filesize(url)
-    r = head_hash(url)
-    return false unless r and r.has_key? 'content-length'
-    r['content-length'].first.to_i
-  end
-
-  def self.remote_etag(url)
-    r = head_hash(url)
-    return false unless r and r.has_key? 'etag'
-    r['etag']
-  end
-      
-  def self.zget(uri)
-        @data = nil
-        
-        begin
-          @userAgent = 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.0.4) Gecko/20060508 Firefox/1.5.0.4'
-          @acceptEncoding = 'gzip'
-          @uri = open(uri, 'rb',
-                     'User-Agent' => @userAgent,
-                     'Accept-Encoding' => @acceptEncoding)
-          if @uri.content_encoding.include? 'gzip'
-            @data = Zlib::GzipReader.new(@uri).read
-          else
-            @data = @uri.read
-          end
-          @uri.close
-        rescue TypeError
-          return nil
-        rescue SocketError
-          return nil
-        rescue OpenURI::HTTPError
-          raise 'HTTPError: 404 Not found'
-        end
-        
-        return @data
-  end
-
 end
