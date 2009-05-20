64aa355d2fb9a2442ed78a710ad06efe6aae01d8
John Leach <john@johnleach.co.uk>
2009-05-20 17:11:05 +0100

    NewsArticleVersion: clean-up

--- app/models/news_article_version.rb
+++ app/models/news_article_version.rb
@@ -16,14 +16,11 @@
 
 #
 class NewsArticleVersion < ActiveRecord::Base
-  belongs_to :news_article
-  before_create :set_new_version
-  after_create :update_latest_hash
-  after_create :inc_counter
-  before_destroy :dec_counter
+  belongs_to :news_article, :counter_cache => 'versions_count'
+  before_validation :set_new_version
   
-  has_many :comments, :conditions => "linktype = 'NewsArticleVersion'",
-    :foreign_key => 'link_id'
+  validates_presence_of :version
+  validates_presence_of :news_article
  
   # populate the object from a NewsPage object
   def populate_from_page(page)
@@ -32,11 +29,6 @@ class NewsArticleVersion < ActiveRecord::Base
     # self.created_at = page.date
     self.url = page.url
     self.text = page.content.join("\n")
-  end
-
-  # news source (via NewsArticle) for ferret indexing
-  def source
-    self.news_article.source
   end
 
   def <=>(b)
@@ -51,17 +43,4 @@ class NewsArticleVersion < ActiveRecord::Base
     self.version = self.news_article.versions_count
   end
   
-  def update_latest_hash
-    self.news_article.update_attribute("latest_text_hash", self.text_hash)
-  end
-  
-  def inc_counter
-    NewsArticle.update_all 'versions_count = versions_count + 1', 
-      "id = #{self.news_article.id}"
-  end
-  
-  def dec_counter
-    NewsArticle.update_all 'versions_count = versions_count - 1', 
-      "id = #{self.news_article.id}"  
-  end
 end
