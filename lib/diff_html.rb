208921936e9588741ae38c4f0e0433118331bdfb
John Leach <john@johnleach.co.uk>
2010-06-07 23:30:16 +0100

    Word level diffing

--- lib/diff_html.rb
+++ lib/diff_html.rb
@@ -1,8 +1,36 @@
 module HTMLDiff
   require 'diff/lcs'
   require 'diff/lcs/callbacks'
-  
-  class HtmlDiff
+  require 'diff/lcs/string'
+        
+  class WordDiff
+    
+    def transpose
+      @words.transpose
+    end
+    
+    def initialize
+      @words = []
+    end
+
+    def match(event)
+      @words << [" #{event.old_element}", " #{event.new_element} "]
+    end
+
+    def discard_a(event)
+      @words << ["<del> #{event.old_element} </del>", ""]
+    end
+
+    def discard_b(event)
+      @words << ["","<ins> #{event.new_element} </ins>"]
+    end
+    
+    def change(event)
+      @words << ["<del> #{event.old_element} </del>", "<ins> #{event.new_element} </ins>"]
+    end
+  end 
+    
+  class ParagraphDiff
 
     def to_html
       @rows.collect { |row| "<tr><td>#{row.first}</td><td>#{row.last}</td></tr>" }.join("\n")
@@ -25,12 +53,17 @@ module HTMLDiff
     end
     
     def change(event)
-      @rows << ["<del>#{event.old_element}</del>", "<ins>#{event.new_element}</ins>"]
+      old = event.old_element.split(/\s/)
+      new = event.new_element.split(/\s/)
+      d = WordDiff.new
+      Diff::LCS.traverse_sequences(old,new,d)
+      old, new = d.transpose
+      @rows << ["<del>#{old.join("")}</del>", "<ins>#{new.join("")}</ins>"]
     end
   end 
 
   def self.diff(a,b)
-    d = HtmlDiff.new
+    d = ParagraphDiff.new
     Diff::LCS.traverse_balanced(a,b,d)
     return d.to_html
   end
