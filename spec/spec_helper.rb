a59b0ce07699c6f122df55f075393f320627af71
John Leach <john@johnleach.co.uk>
2014-09-05 22:33:47 +0100

    Upgrade gems
    Plus tweak to support now deprecated rspec should syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,10 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
 end
 
 def a_news_article(options = { })
