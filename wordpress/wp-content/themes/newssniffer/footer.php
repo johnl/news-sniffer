caa2aa3c01d983a0000bd143447d9d23ae0b8f08
John Leach <john@johnleach.co.uk>
2016-10-05 22:57:14 +0100

    Move new html and css into wordpress theme

--- wordpress/wp-content/themes/newssniffer/footer.php
+++ wordpress/wp-content/themes/newssniffer/footer.php
@@ -3,5 +3,8 @@
         <?php wp_footer(); ?>
       </div>
       </footer>
+   	<script src="http://code.jquery.com/jquery-3.1.0.slim.min.js" integrity="sha256-cRpWjoSOw5KcyIOaZNo4i6fZ9tKPhYYb6i5T9RSVJG8=" crossorigin="anonymous"></script>
+	<script>window.jQuery || document.write('<script src="/javascripts/jquery-3.1.0.slim.min.js"><\/script>')</script>
+	<script src="/assets/application.js"></script>
 </body>
 </html>
--- wordpress/wp-content/themes/newssniffer/header.php
+++ wordpress/wp-content/themes/newssniffer/header.php
@@ -1,31 +1,38 @@
-<!DOCTYPE html> 
-<html> 
-  <head> 
+<!DOCTYPE html>
+<html>
+<head>
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
     <title><?php bloginfo('name'); ?> <?php if ( is_single() ) { ?> &raquo; Archive <?php } ?> <?php wp_title(); ?></title>
-    <link rel="stylesheet" href="/stylesheets/newsniffer.css?100615" type="text/css" media="screen" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <link rel="stylesheet" href="/assets/application.css"" type="text/css" media="screen" />
+	<!--[if lt IE 9]>
+        <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+    <link href="/versions.rss" rel="alternate" title="RSS feed of all news article versions" type="application/rss+xml" />
+
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
     <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 <?php wp_head(); ?>
-  </head>
-  <body>
+</head>
+<body>
     <header>
-    <div id="header">
-      <img src="/stylesheets/layout/newsniffer-text.png" alt="News Sniffer" id="logo"/>
-      <nav>
-  	  <div id="nav"> 
-        <ul>
-      	  <li><a href="/versions">News Article Revisions</a></li>
-      	  <li><a href="/blog/" class="selected">Blog</a></li>
-      	  <li><a href="/blog/about">About</a></li>
-        </ul>
-      </div> 
-      </nav>
-    </div>
+        <div id="header" class="header">
+  			<div class="logo-head">
+				<a href="/"><img src="/images/newsniffer-text.png" alt="News Sniffer" class="logo" id="logo" /></a>
+			</div>
+			<div class="toggle-nav">
+	            <span class="bar"></span>
+	            <span class="bar"></span>
+	            <span class="bar"></span>
+             </div>
+			<nav class="navigation">
+				<ul id="nav" class="menu">
+					<li><a href="https://www.newssniffer.co.uk/versions" title="News article Revisions">News Article Revisions</a></li>
+					<li><a class="selected" href="/blog/">Blog</a></li>
+					<li><a href="/blog/about">About</a></li>
+				</ul>
+			</nav>
     </header>
-    <div id="crossbar">
-    </div>
-      <div id="sidebar">
-        <?php get_sidebar(); ?>      
-      </div>
+    <div id="content" class="content">
+      <div class="wrap">
